ab5ceae2e81bcaffb18eb86676e6b1e7675df3ca
MrCat <1025090504@qq.com>
2022-07-28 11:59:00 +0800

    chore:upgrade bundle function

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Plugin } from 'vite';
-
-/** watch file add/edit then autoExport  */
-declare function export_default(): Plugin;
-
-export { export_default as default };
--- src/plugin/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './autoExport';
-export * from './index';

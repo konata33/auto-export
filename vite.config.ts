35878f94fd215f98ca5536143e251934c2d014f7
MrCat <1025090504@qq.com>
2022-07-31 01:30:42 +0800

    chore:add ci and eslint

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import autoExport from './src/index'
 
 export default defineConfig({
-  plugins: [autoExport({ fileName: 'index', filePath: 'src' })],
+  plugins: [autoExport()],
 })
